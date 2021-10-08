567a4a8a1b0e2fca2b40c4fa8a5afe50ed03912f
Xavier Capaldi <xcapaldi@scribo.biz>
2021-10-08 00:38:15 -0400

    Add support for skeleton markers and moving between them

--- init.el
+++ init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file "~/.config/emacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0f7fa4835d02a927d7d738a0d2d464c38be079913f9d4aba9c97f054e67b8db9" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
