f3beb13db180309f5d456196fdb7125044fe433d
Xavier Capaldi <38892330+xcapaldi@users.noreply.github.com>
2023-07-14 09:53:47 -0400

    Remove elpaca.el from config
    Emacs 28 on MacOS Ventura has a bug related to SVG support. With
    this bug I have to evaluate the generated `init.el` manually after
    first startup so I can't use an alternative package manager that
    relies on disabling `package.el` in the `early-init.el`. This
    commit removes `elpaca.el` and uses the built-in `package.el` for
    this reason. `asdf.el` and `copilot.el` must be cloned and loaded
    locally using this setup.

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -1,148 +1,145 @@
-(defvar elpaca-installer-version 0.5)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(tool-bar-mode -1)
 
-(elpaca elpaca-use-package
-;; Enable :elpaca use-package keyword.
-(elpaca-use-package-mode))
-;; Assume :elpaca t unless otherwise specified.
-;;(setq elpaca-use-package-by-default t))
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
-(elpaca-wait)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
 
 (use-package emacs
-  :elpaca nil
   :config
-  <<LOAD_THEME>>
+  (load-theme 'modus-operandi t)
   (when (equal system-type 'darwin)
-    <<REBIND_MAC_KEYS>>
-    <<MAC_FONTS>>))
+    (setq mac-command-modifier 'meta)
+    (setq mac-option-modifier 'super)
+    (when (member "Monaco" (font-family-list))
+          (add-to-list 'initial-frame-alist '(font . "Monaco-13:regular"))
+          (add-to-list 'default-frame-alist '(font . "Monaco-13:regular")))
+    (set-fontset-font t 'symbol (font-spec :family "Apple Symbols") nil 'prepend)
+    (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend)
+    (setq-default line-spacing 0.1)))
 
 (use-package asdf
+  :load-path "~/Checkout/asdf.el"
   :if (eq system-type 'darwin)
-  :elpaca (asdf :host github
-		:repo "tabfugnic/asdf.el"
-		:branch "main")
   :config (asdf-enable))
+
+(use-package chezmoi
+  :ensure t)
 
 (use-package consult
   ;; Enable automatic preview at point in the *Completions* buffer.
   ;; This is relevant when you use the default completion UI,
   ;; and not necessary for Vertico, Selectrum, etc.
-  :elpaca t
+  :ensure t
   :hook (completion-list-mode . consult-preview-at-point-mode)
   :bind (:map isearch-mode-map
   ("C-c l" . consult-line)))
 
 (use-package copilot
-  :elpaca (copilot :host github
-		   :repo "zerolfx/copilot.el"
-		   :branch "main"
-		   :files ("dist" "*.el"))
+  :load-path "~/Checkout/copilot.el"
   :if (eq system-type 'darwin)
   :hook (prog-mode . copilot-mode))
 
 (use-package corfu
-  :elpaca t
+  :ensure t
   :init
   (global-corfu-mode))
 
 (use-package dumb-jump
-  :elpaca t
+  :ensure t
   :config
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
 (use-package editorconfig
-  :elpaca t
+  :ensure t
   :config (editorconfig-mode 1))
 
 (use-package eglot
-  :elpaca t
+  :ensure t
   :after (project flymake xref)
   :commands eglot)
 
 (use-package exec-path-from-shell
-  :elpaca t
+  :ensure t
   :if (memq window-system '(mac ns x))
   :init (exec-path-from-shell-initialize))
 
 (use-package go-mode
-  :elpaca t
+  :ensure t
   :mode (("\\.go\\'" . go-mode)
          ("\\.mod\\'" . go-dot-mod-mode)))
 
 (use-package js
-  :elpaca nil
+  :ensure nil
   :mode "\\.js[x]\\'")
 
 (use-package magit
-  :elpaca t)
+  :ensure t)
 
 (use-package marginalia
-  :elpaca t
+  :ensure t
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
   :init (marginalia-mode 1))
 
 (use-package orderless
-  :elpaca t
+  :ensure t
   :init
   (setq completion-styles '(orderless basic)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
+(use-package org
+  :config
+  ;; Org-babel supported languages
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((shell . t)
+     (python . t)))
+  :custom
+  (org-adapt-indentation nil)
+  (org-edit-src-content-indentation 0))
+
 (use-package protobuf-mode
-  :elpaca t
+  :ensure t
   :if (eq system-type 'darwin)
   :mode ("\\.proto\\'"))
 
 (use-package terraform-mode
-  :elpaca t
+  :ensure t
   :if (eq system-type 'darwin)
   :mode ("\\.tf\\'" . terraform-mode))
 
 (use-package typescript-mode
-  :elpaca t
+  :ensure t
   :mode "\\.ts[x]\\'")
 
 (use-package vertico
-  :elpaca t
+  :ensure t
   :init (vertico-mode 1))
 
 (use-package vundo
-  :elpaca t
+  :ensure t
   :commands (vundo)
   :bind ("C-c u" . vundo))
+
+(use-package which-key
+  :ensure t
+  :custom
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (which-key-show-early-on-C-h t)
+  ;; Don't show normally but refresh quickly when triggered
+  (which-key-idle-delay 10000)
+  (which-key-idle-secondary-delay 0.05)
+  :init (which-key-mode))
