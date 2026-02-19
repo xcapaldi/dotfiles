6d7e171ccf21bfca420b51e0452564b1264e5871
Xavier Capaldi <38892330+xcapaldi@users.noreply.github.com>
2026-02-19 15:28:05 -0500

    Re-enable CC status bar but more minimal

--- dot_claude/executable_statusline.py
+++ dot_claude/executable_statusline.py
@@ -21,7 +21,7 @@ if output_style == default_output_style:
 else:
     output_style = f"\"{output_style}\" "
 
-# AGENT
+# # AGENT
 # only populated when claude is run with --agent flag or with agent settings
 # claude has a built in way to do this
 # try:
@@ -45,42 +45,41 @@ context_window_used_percentage = int(data.get('context_window', {}).get('used_pe
 
 # String multiplication builds the bar
 filled_context = context_window_used_percentage * 10 // 100
-context_bar = '▓' * filled_context + '░' * (10 - filled_context) + f' {context_window_used_percentage}% '
+context_bar = '[' +  '￭' * filled_context + '･' * (10 - filled_context) + f'] {context_window_used_percentage}% '
 
-# LINES TOUCHED
-
-lines_added = int(data.get('cost', {}).get('total_lines_added', 0) or 0)
-if lines_added == 0:
-    lines_added = ""
-else:
-    lines_added = f"+{lines_added} "
-
-lines_removed = int(data.get('cost', {}).get('total_lines_removed', 0) or 0)
-if lines_removed == 0:
-    lines_removed = ""
-else:
-    lines_removed = f"-{lines_removed} "
-
-
-# DURATION
-def fmt_duration(ms):
-    s = ms // 1000
-    if s < 60:
-        return f"{s}s"
-    m, s = divmod(s, 60)
-    if m < 60:
-        return f"{m}m{s:02d}s"
-    h, m = divmod(m, 60)
-    return f"{h}h{m:02d}m"
-
-total_ms = int(data.get('cost', {}).get('total_duration_ms', 0) or 0)
-api_ms = int(data.get('cost', {}).get('total_api_duration_ms', 0) or 0)
-if total_ms > 0:
-    duration = f"{fmt_duration(total_ms)}({fmt_duration(api_ms)}) "
-else:
-    duration = ""
+# # LINES TOUCHED
+# lines_added = int(data.get('cost', {}).get('total_lines_added', 0) or 0)
+# if lines_added == 0:
+#     lines_added = ""
+# else:
+#     lines_added = f"+{lines_added} "
+# 
+# lines_removed = int(data.get('cost', {}).get('total_lines_removed', 0) or 0)
+# if lines_removed == 0:
+#     lines_removed = ""
+# else:
+#     lines_removed = f"-{lines_removed} "
+# 
+# 
+# # DURATION
+# def fmt_duration(ms):
+#     s = ms // 1000
+#     if s < 60:
+#         return f"{s}s"
+#     m, s = divmod(s, 60)
+#     if m < 60:
+#         return f"{m}m{s:02d}s"
+#     h, m = divmod(m, 60)
+#     return f"{h}h{m:02d}m"
+# 
+# total_ms = int(data.get('cost', {}).get('total_duration_ms', 0) or 0)
+# api_ms = int(data.get('cost', {}).get('total_api_duration_ms', 0) or 0)
+# if total_ms > 0:
+#     duration = f"{fmt_duration(total_ms)}({fmt_duration(api_ms)}) "
+# else:
+#     duration = ""
 
 # OUTPUT
-output = f"{context_bar}{lines_added}{lines_removed}{duration}{model}{output_style}{workspace_current_dir}"
+output = f"{context_bar}{model}{output_style}{workspace_current_dir}"
 if output != "":
     print(output)
